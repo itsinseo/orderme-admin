60e3b02c0293845a56f3e96adf41c2c8fe3733af
Inseo Kim <itsinseo@gmail.com>
2024-01-01 16:35:55 +0900

    init commit

---
+++ src/main/java/org/example/ordermeadmin/OrdermeAdminApplication.java
@@ -0,0 +1,13 @@
+package org.example.ordermeadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermeAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermeAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/ordermeadmin/OrdermeAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ordermeadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermeAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
